b08e11dd8b0194f559a0360b197b91674587e2e6
janja200 <148339622+janja200@users.noreply.github.com>
2024-03-15 12:33:59 +0300

    Add files via upload

--- hooks/messages/useRoutes.ts
+++ hooks/messages/useRoutes.ts
@@ -4,23 +4,45 @@ import { HiChat } from 'react-icons/hi';
 import { HiArrowLeftOnRectangle, HiUsers } from 'react-icons/hi2';
 import { signOut } from "next-auth/react";
 import useConversation from "./useConversation";
+import { useCurrentUser } from "../use-current-user";
+import { BsBellFill, BsHouseFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
 
 const useRoutes = () => {
   const pathname = usePathname();
   const { conversationId } = useConversation();
+  const currentUser = useCurrentUser();
 
   const routes = useMemo(() => [
+    {
+      icon: BsHouseFill,
+      label: 'Home',
+      href: '/home',
+    },
+    {
+      icon: BsBellFill,
+      label: 'Notifications',
+      href: '/notifications',
+      auth: true,
+      alert: currentUser?.isTwoFactorEnabled,
+    },
+    {
+      icon: FaUser,
+      label: 'Profile',
+      href: `/profile/${currentUser?.id}`,
+      auth: true,
+    },
     { 
       label: 'Chat', 
       href: '/conversations', 
       icon: HiChat,
-      active: pathname === '/conversations' || !!conversationId
+      active: pathname === '/conversations' || !!conversationId,
     },
     { 
       label: 'Users', 
       href: '/users', 
       icon: HiUsers, 
-      active: pathname === '/users'
+      active: pathname === '/users',
     },
     {
       label: 'Logout', 
@@ -28,9 +50,10 @@ const useRoutes = () => {
       href: '#',
       icon: HiArrowLeftOnRectangle, 
     }
-  ], [pathname, conversationId]);
+  ], [pathname, conversationId, currentUser]);
 
   return routes;
 };
 
 export default useRoutes;
+
